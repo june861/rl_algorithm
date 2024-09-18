fix softmax bug

ppo/ppo.py
@@ -76,7 +76,7 @@ class Actor(nn.Module):
     def forward(self,state):
         """ actor网络输出动作的概率 """
         out = self.actor(state)
-        a_prob = torch.softmax(out, dim = 0) 
+        a_prob = torch.softmax(out, dim = 1) 
         return a_prob
 
 
@@ -150,12 +150,12 @@ class PPO(object):
         if self.ppo_params['off_policy']:
             self.actor_old = deepcopy(self.actor).to(self.device)
         # define optimizer
-        self.actor_optim = optim.Adam(params = self.actor.parameters(), lr = self.ppo_params['lr_a'])
-        self.critic_optim = optim.Adam(params  = self.critic.parameters(), lr = self.ppo_params['lr_c'])
+        self.actor_optim = optim.AdamW(params = self.actor.parameters(), lr = self.ppo_params['lr_a'])
+        self.critic_optim = optim.AdamW(params  = self.critic.parameters(), lr = self.ppo_params['lr_c'])
 
     def select_action(self, state, eval_mode = False):
         """ 选择动作 """
-        state = torch.Tensor(state).to(self.device)
+        state = torch.Tensor(state).to(self.device).unsqueeze(0)
         with torch.no_grad():
             if self.ppo_params['off_policy'] and eval_mode == False:
                 a_probs = self.actor_old(state).cpu()
@@ -189,7 +189,7 @@ class PPO(object):
             adv = torch.tensor(adv, dtype=torch.float).to(self.device).view(-1, 1)
             # v_target = rewards + self.ppo_params['gamma'] * (1.0 - dones) * value_
             if self.ppo_params['use_adv_norm']:  # Trick 1:advantage normalization
-                adv = ((adv - adv.mean()) / (adv.std() + 1e-5))
+                adv = ((adv - adv.mean()) / (adv.std() + 1e-8))
 
         return adv, v_target
 
@@ -212,7 +212,7 @@ class PPO(object):
         actor_total_loss, critic_total_loss = 0.0, 0.0
         step = 0
 
-        for index in BatchSampler((range(self.ppo_params['batch_size'])), self.ppo_params['mini_batch_size'], False):
+        for index in BatchSampler(SubsetRandomSampler(range(self.ppo_params['batch_size'])), self.ppo_params['mini_batch_size'], True):
             state, next_state, reward, dw, done = states[index], next_states[index], rewards[index], batch_dw[index], dones[index]
             # state = (state - state.mean()) / (state.std() + 1e-8)
             action, a_logprob = actions[index], a_logprobs[index]                                                                                                    
@@ -227,8 +227,9 @@ class PPO(object):
             ratios = torch.exp(a_logprob_now - a_logprob)
             surr1 = ratios * adv  
             surr2 = torch.clamp(ratios, 1 - self.ppo_params['clip_param'], 1 + self.ppo_params['clip_param']) * adv
-            # actor_loss = - torch.min(surr1, surr2) - self.ppo_params['entropy_coef'] * dist_entropy  # shape(mini_batch_size X 1)
-            actor_loss = - torch.min(surr1, surr2)
+            actor_loss = - torch.min(surr1, surr2) - self.ppo_params['entropy_coef'] * dist_entropy  # shape(mini_batch_size X 1)
+            # actor_loss = torch.max(-surr1, -surr2).mean()
+            # actor_loss = - torch.min(surr1, surr2)
             # Update actor
             self.actor_optim.zero_grad()
             actor_loss.mean().backward()

ppo_main.py
@@ -1,9 +1,10 @@
+import gym
+import wandb
 import torch
 import warnings
 warnings.filterwarnings("ignore")
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
-import gym
 import argparse
 from ppo.relaybuffer import RelayBuffer
 from ppo.trick import lr_decay, state_norm, reward_norm, KeepMeanStd
@@ -25,6 +26,7 @@ def main(args, env_name, number, seed):
     args.max_episode_steps = env._max_episode_steps  # Maximum number of steps per episode
     print(f'======== run ppo algorithm =========')
     print("env = {}".format(env_name))
+    print("device = {}".format(device))
     print("state_dim = {}".format(state_dim))
     print("action_dim = {}".format(action_dim))
     print("max_episode_steps = {}".format(args.max_episode_steps))
@@ -53,9 +55,9 @@ def main(args, env_name, number, seed):
             # trick params
             'off_policy' : False, # use off-policy or on-policy
             'use_buffer' : False, # use buffer to store or not  
-            'use_tanh' : False, # use tanh activate func or ReLU func
-            'use_adv_norm' : True, # use advantage normalization
-            'use_grad_clip' : False, # use grad clip in model params.
+            'use_tanh' : args.use_tanh, # use tanh activate func or ReLU func
+            'use_adv_norm' : args.use_adv_norm, # use advantage normalization
+            'use_grad_clip' : args.use_grad_clip, # use grad clip in model params.
             'grad_clip_params': 0.5,
             'entropy_coef': args.entropy_coef,
             'device': device,        
@@ -67,10 +69,13 @@ def main(args, env_name, number, seed):
                 train_params = ppo_params
             )
 
-    # Build a tensorboard 
-    log_dir = 'runs/PPO_{}_number_seed_{}'.format(env_name, number, seed)
-    clear_folder(folder_path = log_dir)
-    writer = SummaryWriter(log_dir=log_dir)
+    if args.monitor == "wandb":
+        wandb.init(project = "ppo")
+    else:
+        # Build a tensorboard 
+        log_dir = 'runs/PPO_{}_number_seed_{}'.format(env_name, number, seed)
+        clear_folder(folder_path = log_dir)
+        writer = SummaryWriter(log_dir=log_dir)
 
     # state_norm_obj = KeepMeanStd(shape = state_dim)
     # action_norm_obj = KeepMeanStd(shape = action_dim)
@@ -86,8 +91,6 @@ def main(args, env_name, number, seed):
                 episode_steps += 1
                 action, a_logprob = agent.select_action(state)  # Action and the corresponding log probability
                 state_, reward, done, trun, _ = env.step(action)
-                if done:
-                    break
                 # if args.use_state_norm:
                 #     state_norm_ = state_norm(state_, state_norm_obj)
                 # if args.use_reward_norm:
@@ -106,8 +109,8 @@ def main(args, env_name, number, seed):
         # When the number of transitions in buffer reaches batch_size,then update
         if len(replay_buffer) >= args.batch_size:
             # print(f'step is {total_steps}, get in train, now buffer len is {len(replay_buffer)}')
-            learn_steps = int(ppo_params['batch_size'] / ppo_params['mini_batch_size'])
-            # learn_steps = 1
+            # learn_steps = int(ppo_params['batch_size'] / ppo_params['mini_batch_size'])
+            learn_steps = 10
             a_total_loss, c_total_loss = 0.0, 0.0
             for _ in range(learn_steps):
                 a_loss, c_loss = agent.learn(replay_buffer = replay_buffer, batch_size = args.batch_size)
@@ -115,13 +118,16 @@ def main(args, env_name, number, seed):
                 c_total_loss += c_loss
             a_loss = a_total_loss / learn_steps
             c_loss = c_total_loss / learn_steps
-            writer.add_scalar(tag = f'actor_loss_{env_name}', scalar_value = a_loss, global_step = optimizer_steps)
-            writer.add_scalar(tag = f'critic_loss_{env_name}', scalar_value = c_loss, global_step = optimizer_steps)
+            if args.monitor == "wandb":
+                wandb.log({'actor_loss': a_loss, 'critic_loss': c_loss})
+            else:
+                writer.add_scalar(tag = f'actor_loss_{env_name}', scalar_value = a_loss, global_step = optimizer_steps)
+                writer.add_scalar(tag = f'critic_loss_{env_name}', scalar_value = c_loss, global_step = optimizer_steps)
             optimizer_steps += 1
             # agent.update_old_net()
             replay_buffer.clear()
 
-        if args.use_lr_decay  :
+        if args.use_lr_decay :
             cur_lr_a = agent.ppo_params['lr_a']
             cur_lr_c = agent.ppo_params['lr_c']
             new_lr_a = lr_decay(agent.actor_optim, cur_step = total_steps, max_step = args.max_train_steps, cur_lr = cur_lr_a)
@@ -131,7 +137,7 @@ def main(args, env_name, number, seed):
 
         # Evaluate the policy every 'evaluate_freq' steps
         if total_steps % args.evaluate_freq == 0:
-            times = 1
+            times = 5
             evaluate_reward = 0
             total_frame = 0
             for j in range(times):
@@ -143,16 +149,19 @@ def main(args, env_name, number, seed):
                     episode_frame += 1
                     action, _  = agent.select_action(state, eval_mode = True)  # We use the deterministic policy during the evaluating
                     state, reward, done,trun, _ = env.step(action)
+                    episode_reward += reward
                     if done: 
                         break
-                    episode_reward += reward
                 total_frame += episode_frame
                 evaluate_reward += episode_reward
             evaluate_rewards.append(evaluate_reward / times)
             evaluate_num += 1
             print(f"total_step is {total_steps}\t evaluate_num:{evaluate_num} \t evaluate_reward:{evaluate_reward / times} \t")
-            writer.add_scalar('step_rewards_{}'.format(env_name), evaluate_reward / times, global_step = evaluate_num)
-            writer.add_scalar('frame_length_{}'.format(env_name), total_frame / times, global_step = evaluate_num)
+            if args.monitor == "wandb":
+                wandb.log({"eval_rewards": (evaluate_reward / times), "eval_steps" : (total_frame / times)})
+            else:
+                writer.add_scalar('step_rewards_{}'.format(env_name), evaluate_reward / times, global_step = evaluate_num)
+                writer.add_scalar('frame_length_{}'.format(env_name), total_frame / times, global_step = evaluate_num)
             
             # Save the rewards
             # if evaluate_num % args.save_freq == 0:
@@ -160,49 +169,51 @@ def main(args, env_name, number, seed):
     writer.close()
 
     # 测试模型
-    round_count = 0
-    while True:
-        round_count += 1
-        state, _ = eval_env.reset()
-        eval_env.render()
-        done = False
-        episode_reward = 0.0
-        step = 0
-        while not done:
-            step += 1
-            action, _  = agent.select_action(state, eval_mode = True)  # We use the deterministic policy during the evaluating
-            state, reward, done,trun, _ = eval_env.step(action)
-            if done: 
-                break
-            episode_reward += reward
-        print(f'round{round_count}: total step is {step}, total reward is {episode_reward}')
+    # round_count = 0
+    # while True:
+    #     round_count += 1
+    #     state, _ = eval_env.reset()
+    #     eval_env.render()
+    #     done = False
+    #     episode_reward = 0.0
+    #     step = 0
+    #     while not done:
+    #         step += 1
+    #         action, _  = agent.select_action(state, eval_mode = True)  # We use the deterministic policy during the evaluating
+    #         state, reward, done,trun, _ = eval_env.step(action)
+    #         if done: 
+    #             break
+    #         episode_reward += reward
+    #     print(f'round{round_count}: total step is {step}, total reward is {episode_reward}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Hyperparameter Setting for PPO")
-    parser.add_argument("--max_train_steps", type=int, default=int(200), help=" Maximum number of training steps")
-    parser.add_argument("--evaluate_freq", type=int, default=20, help="Evaluate the policy every 'evaluate_freq' steps")
+    parser.add_argument("--max_train_steps", type=int, default=int(1000), help=" Maximum number of training steps")
+    parser.add_argument("--evaluate_freq", type=int, default=10, help="Evaluate the policy every 'evaluate_freq' steps")
     parser.add_argument("--save_freq", type=int, default=20,  help="Save frequency")
     parser.add_argument("--batch_size", type=int, default=4096, help="Batch size")
-    parser.add_argument("--mini_batch_size", type=int, default=512, help="Minibatch size")
+    parser.add_argument("--mini_batch_size", type=int, default=500, help="Minibatch size")
     parser.add_argument("--hidden_width", type=int, default=64, help="The number of neurons in hidden layers of the neural network")
     parser.add_argument("--lr_a", type=float, default=1e-3, help="Learning rate of actor")
-    parser.add_argument("--lr_c", type=float, default=1e-4, help="Learning rate of critic")
-    parser.add_argument("--gamma", type=float, default=0.98, help="Discount factor")
-    parser.add_argument("--lamda", type=float, default=1.0, help="GAE parameter")
+    parser.add_argument("--lr_c", type=float, default=3e-4, help="Learning rate of critic")
+    parser.add_argument("--gamma", type=float, default=0.99, help="Discount factor")
+    parser.add_argument("--lamda", type=float, default=0.99, help="GAE parameter")
     parser.add_argument("--epsilon", type=float, default=0.2, help="PPO clip parameter")
     parser.add_argument("--K_epochs", type=int, default=10, help="PPO parameter")
+
+
     parser.add_argument("--use_adv_norm", type=bool, default=True, help="Trick 1:advantage normalization") 
     parser.add_argument("--use_state_norm", type=bool, default=True, help="Trick 2:state normalization")
     parser.add_argument("--use_reward_norm", type=bool, default=True, help="Trick 3:reward normalization")
     parser.add_argument("--use_reward_scaling", type=bool, default=True, help="Trick 4:reward scaling")
-    parser.add_argument("--entropy_coef", type=float, default=0.01, help="Trick 5: policy entropy")
+    parser.add_argument("--entropy_coef", type=float, default=0.1, help="Trick 5: policy entropy")
     parser.add_argument("--use_lr_decay", type=bool, default=True, help="Trick 6:learning rate Decay")
     parser.add_argument("--use_grad_clip", type=bool, default=True, help="Trick 7: Gradient clip")
     parser.add_argument("--use_orthogonal_init", type=bool, default=True, help="Trick 8: orthogonal initialization")
-    parser.add_argument("--set_adam_eps", type=float, default=True, help="Trick 9: set Adam epsilon=1e-5")
-    parser.add_argument("--use_tanh", type=float, default=True, help="Trick 10: tanh activation function")
-
+    parser.add_argument("--set_adam_eps", type=bool, default=True, help="Trick 9: set Adam epsilon=1e-5")
+    parser.add_argument("--use_tanh", type=bool, default=False, help="Trick 10: tanh activation function")
+    parser.add_argument("--monitor",type=str,default="tensorboard",choices=["tensorboard","wandb"],help="use Dynamic tools to monitor train process")
     args = parser.parse_args()
 
     env_name = ['CartPole-v1', 'LunarLander-v2']

ppo_mp/ppo.py
@@ -76,7 +76,7 @@ class Actor(nn.Module):
     def forward(self,state):
         """ actor网络输出动作的概率 """
         out = self.actor(state)
-        a_prob = torch.softmax(out, dim = 0) 
+        a_prob = torch.softmax(out, dim = 1) 
         return a_prob
 
 
@@ -156,8 +156,8 @@ class PPO(object):
         if self.ppo_params['off_policy']:
             self.actor_old = deepcopy(self.actor).to(self.device)
         # define optimizer
-        self.actor_optim = optim.Adam(params = self.actor.parameters(), lr = self.ppo_params['lr_a'])
-        self.critic_optim = optim.Adam(params  = self.critic.parameters(), lr = self.ppo_params['lr_c'])
+        self.actor_optim = optim.AdamW(params = self.actor.parameters(), lr = self.ppo_params['lr_a'])
+        self.critic_optim = optim.AdamW(params  = self.critic.parameters(), lr = self.ppo_params['lr_c'])
 
     def select_action(self, state, eval_mode = False, renturn_entropy = False):
         """ select action
@@ -282,6 +282,8 @@ class PPO(object):
                 self.actor_optim.zero_grad()
                 actor_loss = torch.max(surr1, surr2).mean() - self.ppo_params['entropy_coef'] * entropy_loss
                 actor_loss.backward()
+                if self.ppo_params['use_grad_clip']:  # Trick 7: Gradient clip
+                    nn.utils.clip_grad_norm_(self.actor.parameters(), self.ppo_params['grad_clip_params'])
                 self.actor_optim.step()
 
                 # Value loss
@@ -289,6 +291,8 @@ class PPO(object):
                 self.critic_optim.zero_grad()
                 critic_loss = 0.5 * ((newvalue - b_returns[index]) ** 2).mean()
                 critic_loss.backward()
+                if self.ppo_params['use_grad_clip']:  # Trick 7: Gradient clip
+                    nn.utils.clip_grad_norm_(self.critic.parameters(), self.ppo_params['grad_clip_params'])
                 self.critic_optim.step()
 
                 actor_total_loss += actor_loss.detach().cpu().numpy().item()